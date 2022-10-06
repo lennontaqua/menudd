52c9200fdfc58b4c2c3d0ad7e1dec57935b6124c
Bruna Hidalgo <114705936+hidalgobru@users.noreply.github.com>
2022-10-06 20:26:45 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,6 @@ function updatemenu() {
     document.getElementById('menu').style.borderBottomRightRadius = '0';
     document.getElementById('menu').style.borderBottomLeftRadius = '0';
   }else{
-    document.getElementById('menu').style.borderRadius = '8px';
+    document.getElementById('menu').style.borderRadius = '10px';
   }
 }
